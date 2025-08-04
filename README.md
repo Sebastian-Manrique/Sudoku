0c9bd64fdd66e08f29d9a420984b27cabd0ea995
WildRepublicMini <smanriquemontiel@gmail.com>
2025-08-05 01:47:56 +0200

    2 things
    Added README.md
    
    Created the first screen, this is a very basic design, but it will be better, slowly but surely

---
+++ README.md
@@ -0,0 +1,14 @@
+**Sudoku Game**
+
+<div style="text-align:center;">
+
+[![My Skills](https://skillicons.dev/icons?i=kotlin,androidstudio&theme=dark)](https://skillicons.dev)
+
+</div>
+
+
+This is a Sudokue game created to:
+<ul>
+<li>Learn how to code with Jetpack Compose again</li>
+<li>Try to make my own Sudoku app to avoid ads from the Sudoku games on the market.</li>
+</ul>
--- app/src/main/java/com/ejemplo/sudokuappbysebastian_manrique/MainActivity.kt
+++ app/src/main/java/com/ejemplo/sudokuappbysebastian_manrique/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.ejemplo.sudokuappbysebastian_manrique
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,30 +31,77 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding()
-            ) {
-                val textSample = remember { mutableStateOf("a") }
-                Text(
-                    text = textSample.value, modifier = Modifier.padding()
-                )
-
-                buttonFun(textSample)
-
+            SudokuAppBySebastianManriqueTheme {
+                Column {
+                    SudokuAppBySebastianManriqueTheme {
+                        Board()
+                        SolveButton()
+                    }
+                }
             }
         }
     }
 
     @Composable
-    fun buttonFun(textSample: MutableState<String>) {
-        FilledTonalButton(onClick = {
-            if (textSample.value == "hello world!") textSample.value = "push me!" else textSample.value =
-                "hello world!"
-        }) {
-            Text("Filled")
+    fun Board() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                /*This is the background color for the app for now*/
+                .background(color = MaterialTheme.colorScheme.background)
+                .fillMaxWidth()
+                .height(640.dp)
+                .padding(
+                    top = 50.dp
+                )
+                .absolutePadding(
+                    left = 8.dp, right = 8.dp
+                )
+                .border(
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.onBackground)
+                )
+        ) {
+            Text("Hello World!", color = MaterialTheme.colorScheme.onBackground)
         }
+    }
 
+    @Composable
+    fun SolveButton() {
+        Button(
+            onClick = { callToast() },
+            modifier = Modifier
+                /*This is the background color for the button app for now*/
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(top = 15.dp, bottom = 50.dp)
+                .fillMaxSize()
+                .absolutePadding(
+                    left = 8.dp, right = 8.dp
+                )
+        ) {
+            Text(
+                "Solve",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    }
+
+    private fun callToast() {
+        Toast.makeText(this@MainActivity, "The button was pressed!", Toast.LENGTH_SHORT).show()
+    }
+
+    @Preview(
+        showSystemUi = true,
+        uiMode = Configuration.UI_MODE_NIGHT_YES
+    )
+    @Composable
+    fun SeePreview() {
+        SudokuAppBySebastianManriqueTheme {
+            Column {
+                Board()
+                SolveButton()
+            }
+        }
     }
 }
+
